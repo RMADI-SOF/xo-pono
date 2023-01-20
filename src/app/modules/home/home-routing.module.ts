9ec1ead4825d7fa0c7b1f95fd2efc20be3134b2a
sofiene <sofienebboy@gmail.com>
2023-01-20 13:50:23 +0100

    feat: reactivate guards

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IsNotSignedInGuard } from 'src/app/core/guards/is-not-signed-in.guard';
+import { IsSignedInGuard } from 'src/app/core/guards/is-signed-in.guard';
 import { BoardComponent } from './components/board/board.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [
+const routes: Routes = 
+[
   { path: 'home', component: HomeComponent,
-    // canActivate: [IsSignedInGuard]
+    canActivate: [IsSignedInGuard]
   },
-  { path: 'board', component: BoardComponent},
+  { path: 'board', component: BoardComponent,
+    canActivate: [IsSignedInGuard]
+  },
   { path: 'register', component: RegisterComponent,
-  // canActivate: [IsNotSignedInGuard] 
-  },
+    canActivate: [IsNotSignedInGuard] 
+  }
 ];
 
 @NgModule({
